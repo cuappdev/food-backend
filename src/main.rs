8fe70cc6faaa85d21a357bb3e5fee113e2e8f192
sn685 <shungo.najima@gmail.com>
2021-03-28 17:18:30 -0400

    Added rudimentary test

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,23 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{http, test};
+
+    #[test]
+    fn one_eq_one() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    #[actix_rt::test]
+    async fn test_index_ok() {
+        let mut app = test::init_service(App::new().service(hello)).await;
+
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert_eq!(resp.status(), http::StatusCode::OK);
+    }
+}
